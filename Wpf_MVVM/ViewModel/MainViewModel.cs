99151deeeb7e168326295b30e825194e125d8b9b
nb <disha07@mail.ru>
2021-07-08 19:34:02 +0300

    ---

---
+++ Wpf_MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Wpf_MVVM.Model;
+using System.Windows;
+using System.Windows.Input;
+namespace Wpf_MVVM.ViewModel
+{
+    class MainViewModel
+    {
+
+
+        #region Constructor
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainViewModel()
+        {
+            People = new PeopleModel
+ {
+ FirstName = "First name",
+ LastName = "Last name"
+ };
+        }
+        #endregion
+        #region Properties
+        /// <summary>
+        /// Get or set people.
+        /// </summary>
+        public PeopleModel People { get; set; }
+        #endregion
+    }
+}
