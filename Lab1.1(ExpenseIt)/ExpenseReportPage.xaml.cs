9e08d8644555266f271098ea91f5a2e0ddfe9d57
nb <disha07@mail.ru>
2021-07-08 17:24:03 +0300

    Упражнение 1. Создание инфраструктуры приложения

--- Lab1.1(ExpenseIt)/ExpenseReportPage.xaml.cs
+++ Lab1.1(ExpenseIt)/ExpenseReportPage.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace Lab1._1_ExpenseIt_
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для ExpenseReportPage.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class ExpenseReportPage : Page
     {
-        public MainWindow()
+        public ExpenseReportPage()
         {
             InitializeComponent();
         }
